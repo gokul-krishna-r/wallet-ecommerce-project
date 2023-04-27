61da194f8f0667013427ef159fe6bf5fdbf76345
Gokul <gokulkrishna.gk32@gmail.com>
2023-04-28 04:27:28 +0530

    Initial commit

---
+++ models/user.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt-nodejs");
+const Schema = mongoose.Schema;
+
+const userSchema = Schema({
+  username: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  credit:{
+    type:Number,
+    default:1000,
+    require:true
+    
+  }
+});
+
+// encrypt the password before storing
+userSchema.methods.encryptPassword = (password) => {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(5), null);
+};
+
+userSchema.methods.validPassword = function (candidatePassword) {
+  if (this.password != null) {
+    return bcrypt.compareSync(candidatePassword, this.password);
+  } else {
+    return false;
+  }
+};
+
+module.exports = mongoose.model("User", userSchema);
